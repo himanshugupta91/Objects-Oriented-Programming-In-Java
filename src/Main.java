3c8b07ae85d972f2950d908bc7b71a6dc3a924b8
himanshu <hg1480144@gmail.com>
2025-09-02 18:16:04 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 
 public class Main {
-    public static void main(String[] args) {
+    String name;
+    int roll_no;
 
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.roll_no = 14;
+        main .name = "Himanshu";
+        System.out.println(main.name);
+        System.out.println(main.roll_no);
         }
     }
