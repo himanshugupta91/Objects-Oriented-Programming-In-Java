87f1e2506b03487b5ca557ade84cbb34fb383dae
himanshu <hg1480144@gmail.com>
2025-09-03 15:42:22 +0530

    init commit

---
+++ src/Demoo.java
@@ -0,0 +1,14 @@
+ class Student{
+    String name;
+    int age;
+    String address;
+}
+public class Demoo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name="Himanshu";
+        System.out.println(s1.name);
+        s1.name = "manish";
+        System.out.println(s1.name);
+    }
+}
