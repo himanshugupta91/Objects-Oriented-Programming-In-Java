abd9b26eda22ee520c6607cff39ae0af8868e20c
himanshu <hg1480144@gmail.com>
2025-09-03 16:55:29 +0530

    init commit

---
+++ src/Constructor.java
@@ -0,0 +1,15 @@
+class Students{
+    int id;
+    int age;
+    Students(int id, int age){
+        this.id = id;
+        this.age=age;
+        System.out.println("Hello World");
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args) {
+    Students students=new Students(1,20);
+    }
+}
