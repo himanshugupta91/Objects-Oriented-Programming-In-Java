298a6e0d9fe340d27d279cb9ce57bccd068997a9
himanshu <hg1480144@gmail.com>
2025-09-05 17:13:55 +0530

    init commit

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -1,20 +1,34 @@
-class parentClass{
+class vehicle{
+    double price;
+    double mileage;
+    String color;
     void display(){
-        System.out.println("parent class method");
+        System.out.println(price);
+        System.out.println(mileage);
+        System.out.println(color);
     }
 }
 
-class childClass extends parentClass{
-    void show(){
-        System.out.println("child class method");
-    }
+class car extends vehicle{
+    String fType;
+    boolean sunRoof;
+    String Brand;
+
 }
 
 
 public class Inheritance {
     public static void main(String[] args) {
-        childClass childClass=new childClass();
-        childClass.display();
-        childClass.show();
+        car car1=new car();
+        car1.Brand = "tata";
+        car1.color = "blue";
+        car1.mileage=1200;
+        car1.price =50000000;
+        car1.sunRoof=true;
+        car1.fType="Diesel";
+
+        car1.display();
+        System.out.println(car1.sunRoof);
+
     }
 }
