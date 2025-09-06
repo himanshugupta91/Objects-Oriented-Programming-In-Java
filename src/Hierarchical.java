a93367c78e9aff385e60a0682ab34a0e82eefe38
himanshu <hg1480144@gmail.com>
2025-09-06 10:46:29 +0530

    init commit

--- src/Hierarchical.java
+++ src/Hierarchical.java
@@ -1,2 +1,28 @@
+class Q{
+    public void print(){
+        System.out.println("class Q methode");
+    }
+}
+
+class W extends Q{
+    public void printW(){
+        System.out.println("class W methode");
+    }
+}
+
+class E extends Q{
+    public void printE(){
+        System.out.println("class E methode");
+    }
+}
+
+
 public class Hierarchical {
+    public static void main(String[] args) {
+        W obj = new W();
+        E obj2 = new E();
+        obj.print();
+        obj.print();
+
+    }
 }
