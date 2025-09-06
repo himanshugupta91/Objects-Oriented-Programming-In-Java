55177440cd52c837ae3c3aac17c28037caaba550
himanshu <hg1480144@gmail.com>
2025-09-06 10:01:30 +0530

    init commit

--- src/Interface.java
+++ src/Interface.java
@@ -1,3 +1,25 @@
-public class Interface {
+interface Car{
+    public void start();
+}
 
+class ElectricCar implements Car{
+    public void start(){
+        System.out.println("ElectricCar starts");
+    }
+}
+
+class DieselCar implements Car{
+    public void start(){
+        System.out.println("DieselCar starts");
+    }
+}
+
+
+public class Interface {
+    public static void main(String[] args) {
+    Car tesla = new ElectricCar();
+    Car XUV700 = new DieselCar();
+    tesla.start();
+    XUV700.start();
+    }
 }
